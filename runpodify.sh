f82e1190afc6adeaca107c870d6f93d6e30ada27
AdityaShankar <me@adityashankar.xyz>
2023-04-11 04:50:42 +0530

    better test_input.json for now

--- runpodify.sh
+++ runpodify.sh
@@ -1,7 +1,5 @@
 pip install runpod
-
 wget https://raw.githubusercontent.com/therealadityashankar/replicate2runpod/main/runpod_infer.py
 wget https://raw.githubusercontent.com/therealadityashankar/replicate2runpod/main/test_input.json
-
 python -m cog.server.http &
 python runpod_infer.py
